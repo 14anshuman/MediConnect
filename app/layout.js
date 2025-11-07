8c0795beb5242ecc639cb2d398a945aa377cc062
Anshuman Singh <singhanshuman8182@gmail.com>
2025-11-07 16:46:48 +0530

    theme

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,41 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Doctors Appointment App",
+  title: "Medi Connect",
   description: "Connect with doctors anytime, anywhere",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        
+        className={`${inter.className}`}
       >
-        {children}
+        {/* Header */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <footer className="bg-muted/50 py-8">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made with 💗 by Anshuman</p>
+            </div>
+          </footer>
+        </ThemeProvider>
+
+        {/* Footer */}
+
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      Hello How are you
-      Hello
+      hrloo
+      <Button>Click me</Button>
     </div>
   );
 }
